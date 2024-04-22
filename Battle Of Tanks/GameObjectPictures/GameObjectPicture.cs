22c714c6155f1cf4309f2c4afaeb16b48ad10d91
Satana <135997444+Satana6699@users.noreply.github.com>
2024-04-22 22:06:05 +0300

    ну типо гг

--- Battle Of Tanks/GameObjectPictures/GameObjectPicture.cs
+++ Battle Of Tanks/GameObjectPictures/GameObjectPicture.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Battle_Of_Tanks.GameObjectPictures
 {
@@ -22,11 +23,13 @@ namespace Battle_Of_Tanks.GameObjectPictures
             PictureBox.Location = new System.Drawing.Point(GameObject.Position.Point.X, GameObject.Position.Point.Y);
             PictureBox.Width = GameObject.Position.Width;
             PictureBox.Height = GameObject.Position.Height;
+            PictureBox.Padding = new Padding(0);
+            PictureBox.Margin = new Padding(0);
 
-            PictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            PictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
             PictureBox.BackColor = Color.Transparent;
 
-            //PictureBox.BorderStyle = BorderStyle.FixedSingle;
+            PictureBox.BorderStyle = BorderStyle.None;
         }
 
     }
